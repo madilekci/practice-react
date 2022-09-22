6f457237eecb0bf1f3447a2307317f34d8620acc
Muhammed Ali <madilekci@gmail.com>
2022-09-22 22:23:13 +0300

    Customize stylings
    Use bootstrap components and customize its style with css

--- one-video/src/App.js
+++ one-video/src/App.js
@@ -3,8 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className="container">
-      <h5>Title</h5>
-      <p>This is sample text</p>
+      <h5>One Video App</h5>
+      <button className='btn btn-primary'> This is a primary button</button>
+      <button className='btn btn-secondary'> This is a secondary button</button>
+      <button className='btn btn-info'> This is a info button</button>
+      <button className='btn btn-success'> This is a success button</button>
+      <button className='btn btn-warning'> This is a warning button</button>
+      <button className='btn btn-danger'> This is a danger button</button>
+      <hr/>
     </div>
   );
 }
