89b49ee10e793438ad6a307c382d42aaed808e23
Muhammed Ali <madilekci@gmail.com>
2022-09-22 22:41:43 +0300

    Pass users array as props to the UsersTable

--- one-video/src/App.js
+++ one-video/src/App.js
@@ -5,13 +5,32 @@ import AddUserForm from './components/add-user-form';
 import UsersTable from './components/users-table/users-table';
 
 function App() {
+
+  let users = [
+    {
+      id: "1",
+      name: "Muhammed Ali Dilekçi",
+      email: "madilekci@gmail.com",
+    },
+    {
+      id: "2",
+      name: "Barış Pehlivan",
+      email: "byildirim@gmail.com",
+    },
+    {
+      id: "3",
+      name: "Uğur Pehlivan",
+      email: "upehlivan@gmail.com",
+    }
+  ]
+
   return (
     <div className="container">
       <h5>One Video App</h5>
       <hr/>
       <AddUserForm/>
       <hr/>
-      <UsersTable/>
+      <UsersTable users = {users} />
     </div>
   );
 }
