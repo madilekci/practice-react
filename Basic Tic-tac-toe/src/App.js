3cca3a92dbf94a871bde27664300742e3d5e3551
Muhammed Ali <madilekci@gmail.com>
2023-01-25 16:12:50 +0300

    Create board for game

--- Basic Tic-tac-toe/src/App.js
+++ Basic Tic-tac-toe/src/App.js
@@ -1,4 +1,23 @@
 import React from 'react';
-export default function Square() {
-  return <button className="square">X</button>;
+export default function Board() {
+
+  return (
+    <div>
+      <div className="board-row">
+        <button className="square">1</button>
+        <button className="square">2</button>
+        <button className="square">3</button>
+      </div>
+      <div className="board-row">
+        <button className="square">4</button>
+        <button className="square">5</button>
+        <button className="square">6</button>
+      </div>
+      <div className="board-row">
+        <button className="square">7</button>
+        <button className="square">8</button>
+        <button className="square">9</button>
+      </div>
+    </div>
+  );
 }
