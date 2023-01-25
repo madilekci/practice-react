5672527d7064303bf5d316430ae209c68eefc34e
Muhammed Ali <madilekci@gmail.com>
2023-01-25 16:43:57 +0300

    Create Square component

--- Basic Tic-tac-toe/src/App.js
+++ Basic Tic-tac-toe/src/App.js
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+function Square () {
+  const handleSquareClick = () => {
+    console.log(`Square ${value} is clicked`)
+    setValue('X');
+  }
+  const [value, setValue] = useState(null);
+  return <button onClick={handleSquareClick} className="square">{value}</button>
+}
+
 export default function Board() {
 
   return (
     <div>
       <div className="board-row">
-        <button className="square">1</button>
-        <button className="square">2</button>
-        <button className="square">3</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <button className="square">4</button>
-        <button className="square">5</button>
-        <button className="square">6</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <button className="square">7</button>
-        <button className="square">8</button>
-        <button className="square">9</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
     </div>
   );
