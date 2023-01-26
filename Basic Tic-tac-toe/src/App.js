fb2d51cb1e172f3d8f1c08074d1e60b10dee469c
Muhammed Ali <madilekci@gmail.com>
2023-01-26 11:22:05 +0300

    Create top level Game component

--- Basic Tic-tac-toe/src/App.js
+++ Basic Tic-tac-toe/src/App.js
@@ -7,51 +7,29 @@ function Square ({ value, onSquareClick }) {
   return <button onClick={onSquareClick} className="square">{value}</button>
 }
 
-export default function Board() {
-  const players = [
-    {
-      id: 0,
-      symbol: 'X',
-      name: 'Player 1'
-    },
-    {
-      id: 1,
-      symbol: 'O',
-      name: 'Player 2'
-    }
-  ]
-
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  // used to determine whose turn to play, player1 or player2
-  const [currentPlayer, setCurrentPlayer] = useState(players[0])
-
-  const winner = calculateWinner(squares, players)
+function Board({squares, onMove, currentPlayer}) {
 
   const handleSquareClick = i => {
-
-    // do nothing if square is already filled with a symbol or someone won the game
-    if (squares[i] || calculateWinner(squares, players)) {
+    // do nothing if square is already filled with a symbol
+    if (squares[i]) {
       return;
     }
-
     // Create a copy of state
     const nextSquares = squares.slice();
 
     // update the clicked square's value to current player's symbol if the square is empty
     nextSquares[i] = currentPlayer.symbol;
 
-    setSquares(nextSquares);
+    onMove(nextSquares)
+    // setSquares(nextSquares);
 
     // flip the currentUser
-    currentPlayer.id == 0 ? setCurrentPlayer(players[1]) : setCurrentPlayer(players[0]);
+    // currentPlayer.id == 0 ? setCurrentPlayer(players[1]) : setCurrentPlayer(players[0]);
 
   }
 
   return (
     <div>
-      <div style={{marginBottom: '40px', padding:'20px', border: '1px solid', display: 'inline-block'}}>
-        { `${ currentPlayer.id } | ${ currentPlayer.name } | ${ currentPlayer.symbol }` }
-      </div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleSquareClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleSquareClick(1)} />
@@ -67,12 +45,58 @@ export default function Board() {
         <Square value={squares[7]} onSquareClick={() => handleSquareClick(7)} />
         <Square value={squares[8]} onSquareClick={() => handleSquareClick(8)} />
       </div>
-      {
-        winner &&
-        <div style={{marginTop: '40px', padding:'20px', border: '1px solid', display: 'inline-block'}}>
-          { `${winner.name} won the game` }
+    </div>
+  );
+}
+
+export default function Game() {
+  const players = [
+    {
+      id: 0,
+      symbol: 'X',
+      name: 'Player 1'
+    },
+    {
+      id: 1,
+      symbol: 'O',
+      name: 'Player 2'
+    }
+  ]
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  // used to determine whose turn to play, player1 or player2
+  const [currentPlayer, setCurrentPlayer] = useState(players[0])
+
+  const currentSquares = history[history.length - 1];
+  const winner = calculateWinner(currentSquares, players)
+
+
+  function handleMove(nextSquares){
+    // do nothing if someone won the game
+    if (calculateWinner(currentSquares, players)) {
+      return;
+    }
+    setHistory([...history, nextSquares]);
+
+    // flip the currentUser
+    currentPlayer.id == 0 ? setCurrentPlayer(players[1]) : setCurrentPlayer(players[0]);
+  }
+
+  return (
+    <div className='container'>
+      <div className="game">
+        <div className='playerInfo'>
+          { `${ currentPlayer.name } | ${ currentPlayer.symbol }` }
         </div>
-      }
+        <div className="gameBoard">
+          <Board squares={currentSquares} onMove={handleMove} currentPlayer={currentPlayer} />
+        </div>
+        {
+          winner &&
+          <div className='winnerCard'>
+            { `${winner.name} won the game` }
+          </div>
+        }
+      </div>
     </div>
   );
 }
