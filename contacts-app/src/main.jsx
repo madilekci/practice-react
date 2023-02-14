5ffac00d806d08588dda06b555a8163d1259d765
Muhammed Ali <madilekci@gmail.com>
2023-02-14 15:22:45 +0300

    Add react-router and a root route

--- contacts-app/src/main.jsx
+++ contacts-app/src/main.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import Root from "./routes/root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
