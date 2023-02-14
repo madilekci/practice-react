0e4c009ffd3dcdaeef4e125dcc3d880705b21891
Muhammed Ali <madilekci@gmail.com>
2023-02-14 23:07:08 +0300

    Update contact.jsx

--- contacts-app/src/routes/contact.jsx
+++ contacts-app/src/routes/contact.jsx
@@ -81,6 +81,10 @@ export default function Contact() {
 function Favorite({ contact }) {
   const fetcher = useFetcher();
   let favorite = contact.favorite;
+  if (fetcher.formData) {
+    favorite = fetcher.formData.get("favorite") === "true";
+  }
+  
   return (
     <fetcher.Form method="post">
       <button
